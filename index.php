32731893c2826d94b8c8c51cba94187e11377717
Joris Mertz <35079666+JorisMertz@users.noreply.github.com>
2018-11-04 18:39:55 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,13 @@ $domain = "https://thiqq.life/";
 $panel = "panel.php";
 
 $str = $_SERVER['QUERY_STRING'];
-function random_color_part(){return str_pad(dechex(mt_rand(0, 255)), 2, '0', STR_PAD_LEFT);}function random_color(){return random_color_part() . random_color_part() . random_color_part();}
+function randomINT() {
+    return str_pad(dechex(mt_rand(0, 255)), 2, '0', STR_PAD_LEFT);
+}
+function randomRGB(){
+    return randomINT() . randomINT() . randomINT();
+    
+}
 
 function endsWith($t, $s) {
     $length = strlen($s);
@@ -14,14 +20,15 @@ function endsWith($t, $s) {
 
 if (!empty($_GET)) {
 if (strpos($_SERVER['HTTP_USER_AGENT'], 'Discordbot') !== false) {
-echo '<head><title>' . $str. '</title><meta content="summary_large_image" name="twitter:card"><meta content="' . $str . '" property="og:title"><meta content="#' . random_color() . '"name="theme-color"><meta content="' . $domain . $path . $str. '"name="twitter:image:src"></head>';
+echo '<head><title>' . $str. '</title><meta content="summary_large_image" name="twitter:card"><meta content="' . $str . '" property="og:title"><meta content="#' . randomRGB() . '"name="theme-color"><meta content="' . $domain . $path . $str. '"name="twitter:image:src"></head>';
 }
 else {
     if (endsWith($str, "png")) {header("Content-type: image/png");}
     if (endsWith($str, "gif")) {header("Content-type: image/gif");}
     if (endsWith($str, "mp4")) {header("Content-type: video/mp4");}
     echo readfile($path.$str);
-} return; } else {
+} return;
+} else {
     if (file_exists($panel)) {
     header('Location: ' . $panel);
     } else {
